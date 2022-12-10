d4856236b34e33a8c1cec7ff09c011dcec03e449
shu1513 <shu_1315@hotmail.com>
2022-12-10 04:12:08 +0000

    Sat, Dec 10, 2022, 4:12 AM +00:00

--- parity.c
+++ parity.c
@@ -2,5 +2,13 @@
 #include <stdio.h>
 
 int main(void) {
-    
+    int number = get_int ("what is the number you want to enter? ");
+    if (number % 2 == 0)
+    {
+        printf("this is an even number\n");
+    }
+    else
+    {
+        printf("This is an odd number.\n");
+    }
 }

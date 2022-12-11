587a443b216658f19a977c48f014401bc471989f
shu1513 <shu_1315@hotmail.com>
2022-12-11 23:12:57 +0000

    Sun, Dec 11, 2022, 11:12 PM +00:00

--- discount.c
+++ discount.c
@@ -7,11 +7,12 @@ float discount(float y);
 int main (void)
 {
     float x = get_float("Regular price: ");
-    float z = discount(x);
-    printf("sale price is %.2f\n", z);
+    float z = get_float("How many percent off for sales discount ");
+    float a = x * (100-z)/100;
+    printf("sale price is %.2f\n", a);
 }
 
-float discount(float y)
+float discount_conversion(float y)
 {
     return y *.85;
 }

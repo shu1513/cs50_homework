c4a547b04d788813131ed8c18b83b66363b77496
shu1513 <shu_1315@hotmail.com>
2022-12-11 22:43:24 +0000

    Sun, Dec 11, 2022, 10:43 PM +00:00

--- discount.c
+++ discount.c
@@ -1,14 +1,15 @@
 #include <CS50.h>
 #include<stdio.h>
 
+
+float discount(float price);
+
 int main (void)
 {
-
+    float regular_price = get float("Regular price: ")
 }
 
 float discount(flaot price)
 {
-    float sale = price *.85;
-    return sale;
-    
+    return price *.85;
 }

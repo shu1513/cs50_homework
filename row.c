a22382c4eb3296124e09fee0764c2fed0a6e5de0
shu1513 <shu_1315@hotmail.com>
2022-12-13 01:44:32 +0000

    Tue, Dec 13, 2022, 1:44 AM +00:00

--- row.c
+++ row.c
@@ -12,7 +12,9 @@ int main (void)
 
     for (int i = 0; i < n; i++)
     {
-        printf("#");
+        for (int j = 0; j < n; j ++)
+        {
+            printf{"#/n"}
+        }
     }
-    printf("\n");
 }

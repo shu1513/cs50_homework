02b88d5fb046331a703cedcc66ed931088f7723f
shu1513 <shu_1315@hotmail.com>
2022-12-17 04:24:32 +0000

    Sat, Dec 17, 2022, 4:24 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -12,11 +12,9 @@ int main (void)
 
     for (int i = 1; i <= n; i++)
     {
-        int j = n-1;
-        while (j>=1)
+        for(int j = n - 1 ; j>=1;j--)
         {
             printf("a");
-            j--;
         }
         printf("#\n");
     }

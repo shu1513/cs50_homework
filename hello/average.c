ca163fcd1d6bb774c22d9e1d235975531046beb7
shu1513 <shu_1315@hotmail.com>
2022-12-24 23:17:18 +0000

    Sat, Dec 24, 2022, 11:17 PM +00:00

--- hello/average.c
+++ hello/average.c
@@ -1,6 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
+
+int get_number_of_exams (void);
+int get_average_scores (number_of_exams);
+
 int main(void)
 {
-    
+   int number_of_exams = int get_number_of_exams (void);
+   int average_score = int get_average_scores (number_of_exams);
+   printf ("Your average score is %i\n", average_score);
 }
+
+int get_number_of_exams (void)
+{
+   int exam_times;
+   do {exam_times = get_int ("How many exams did you take? ")}
+   while (exam_times < 0)
+}
+

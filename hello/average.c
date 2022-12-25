21249ef697a67358858e2c87fbdeed1bdf7b544e
shu1513 <shu_1315@hotmail.com>
2022-12-25 00:04:17 +0000

    Sun, Dec 25, 2022, 12:04 AM +00:00

--- hello/average.c
+++ hello/average.c
@@ -14,7 +14,10 @@ int main(void)
 int get_number_of_exams (void)
 {
    int exam_times;
-   do {exam_times = get_int ("How many exams did you take? ");}
+   do {exam_times = get_int ("How many exams did you take? ");
+   if (exam_times < 0) {
+                printf("Please enter a non-negative number.\n");
+            }
    while (exam_times < 0);
    return exam_times;
 }
@@ -24,8 +27,11 @@ double get_average_scores (int times_of_exams)
     int sum = 0;
     for (int i = 0; i < times_of_exams; i++)
     {
-        do {int score = get_int ("Whats your score for exam %i ?", (i+1));}
-        while score < 0
+        do {int score = get_int ("Whats your score for exam %i ?", (i+1));
+        if (score < 0) {
+                printf("Please enter a non-negative number.\n");
+            }
+        while (score < 0)
         sum = sum + score
     }
     double average = sum/times_of_exams;

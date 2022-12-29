f430f55731a233288bac64f0bb2a7c51f01c5270
shu1513 <shu_1315@hotmail.com>
2022-12-29 01:30:26 +0000

    Thu, Dec 29, 2022, 1:30 AM +00:00

--- hello/hello2.c
+++ hello/hello2.c
@@ -6,7 +6,7 @@ int main (void)
 
    string s = get_string ("Type input here  ");
    printf("Out put:  ");
-   for (int i = 0; i < strlen(s); i++)
+   for (int i = 0, n = strlen(s); i < n; i++)
    {
       printf("%c",s[i]);
    }

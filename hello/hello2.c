c8236fecfaaaaf6fd36e7356cbea1e9c5a0b4a20
shu1513 <shu_1315@hotmail.com>
2022-12-30 06:16:02 +0000

    Fri, Dec 30, 2022, 6:16 AM +00:00

--- hello/hello2.c
+++ hello/hello2.c
@@ -4,11 +4,13 @@
 
 
 float average (int x[])
+int get_number_of_exams (void)
 
 int main (void)
 {
 
-   int number_of_exams = get_number_of_exams;
+   int number_of_exams = get_number_of_exams (void);
    float average_score = average (number_of_exams);
    printf("%f\n", average_score);
 }
+

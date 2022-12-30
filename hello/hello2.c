b777ac6284f1d8472c3d6753a2a9fbd6542b072d
shu1513 <shu_1315@hotmail.com>
2022-12-30 07:06:11 +0000

    Fri, Dec 30, 2022, 7:06 AM +00:00

--- hello/hello2.c
+++ hello/hello2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <string.h>
 
 
 float average (int x[]);
@@ -9,7 +8,7 @@ int get_number_of_exams (void);
 int main (void)
 {
 
-   int number_of_exams = get_number_of_exams (void);
+   int number_of_exams = get_number_of_exams ();
    float average_score = average (number_of_exams);
    printf("%f\n", average_score);
 }

e33befc674d6bf14e4d68376738c92312fae439a
shu1513 <shu_1315@hotmail.com>
2022-12-31 03:30:41 +0000

    Sat, Dec 31, 2022, 3:30 AM +00:00

--- hello/length.c
+++ hello/length.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int get_string_length(string x)
+
 int main (void)
 {
     string s = get_string ("type your string here ")
     printf("your string has %i characters.\n", get_string_length(s))
+}
+
+int get_string_length(string x)
+{
+    
 }

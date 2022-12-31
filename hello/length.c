a79b23a90ce5aa4e16ff919fad83ef274b3478f9
shu1513 <shu_1315@hotmail.com>
2022-12-31 03:03:18 +0000

    Sat, Dec 31, 2022, 3:03 AM +00:00

---
+++ hello/length.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    string s = get_string ("type your string here ")
+    printf("your string has %i characters.\n", get_string_length(s))
+}

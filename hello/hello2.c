d7ceeaf8b7b99ca6558c885a4ee0a95ecf35e00e
shu1513 <shu_1315@hotmail.com>
2022-12-31 01:06:40 +0000

    Sat, Dec 31, 2022, 1:06 AM +00:00

--- hello/hello2.c
+++ hello/hello2.c
@@ -2,37 +2,49 @@
 #include <cs50.h>
 
 
-float average (int x[]);
+float average (int y, int x[]);
 int get_number_of_exams (void);
 
 int main (void)
 {
 
    int number_of_exams = get_number_of_exams ();
-   int scores = getscores(x[])
-   printf("Average score is %f\n", average());
+   int scores[number_of_exams];
+   for (int i =0; i <number_of_exams; i++)
+   {
+     do {
+      scores[i] = get_int ("Exam %i Score: ", (i+1));
+      if (scores[i] <= 0)
+      {
+         printf("Score needs to be a positive number\n");
+      }
+      }
+      while (scores[i] <= 0);
+   }
+   printf("Average score is %f\n", average(number_of_exams, scores));
 }
 
 int get_number_of_exams (void)
 {
+   int n;
    do
    {
-     int n = get_int ("How many exams did you take?  ");
+     n = get_int ("How many exams did you take?  ");
      if (n <= 0)
      {
-      printf("The number needs to be a positive number");
+      printf("The number needs to be a positive number\n");
      }
    }
-   while (n <= 0)
+   while (n <= 0);
    return n;
 }
 
-float average (int x[])
+float average (int y, int x[])
 {
    int sum = 0;
-   for (i =0; i < number_of_exams, i++)
+   for (int i =0; i < y; i++)
    {
-      sum = sum + x[number_of_exams];
+      sum = sum + x[i];
    }
-   return sum/ ((float) number_of_exams);
+   return sum/ ((float) y);
 }

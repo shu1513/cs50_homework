a69ae1a6ddc285ccc854b8e8f33b9a9edfe51093
shu1513 <shu_1315@hotmail.com>
2023-01-01 21:19:20 +0000

    Sun, Jan 1, 2022, 9:19 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -9,8 +9,14 @@ int main (void)
     int sides[3]
     for (int i = 0; i < 3; i++)
     {
-        sides[i] = get_int("What's the length of side %i ?  ", (i+1))
-
+        do {
+            sides[i] = get_int("What's the length of side %i ?  \n", (i+1));
+            if (side[i] <= 0);
+            {
+                printf("side %i lengh needs to be a positive integer.", (i+1))
+            }
+            }
+        while (sides[i] <= 0;)
     }
 
 }

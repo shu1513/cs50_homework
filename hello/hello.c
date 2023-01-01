8b30b2f362769c28c422812e785cce7cf60d0b3b
shu1513 <shu_1315@hotmail.com>
2023-01-01 21:38:01 +0000

    Sun, Jan 1, 2022, 9:38 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -2,27 +2,34 @@
 #include <cs50.h>
 #include <string.h>
 
-int get_3_sides (void);
+string check_valid (int array[]);
 
 int main (void)
 {
-    int sides[3]
+    int sides[3];
     for (int i = 0; i < 3; i++)
     {
         do {
             sides[i] = get_int("What's the length of side %i ?  \n", (i+1));
-            if (side[i] <= 0);
+            if (sides[i] <= 0)
             {
-                printf("side %i lengh needs to be a positive integer.", (i+1))
+                printf("side %i lengh needs to be a positive integer.", (i+1));
             }
             }
-        while (sides[i] <= 0;)
+        while (sides[i] <= 0);
     }
-    bool valid = check_valid (int array[])
+    string x;
+    x = check_valid (sides);
+    printf("%s\n", x);
+}
 
-}
-int get_3_sides (void)
+string check_valid (int array[])
 {
-   for
+    if (array[0]+ array[1]) > array[2] && (array[1] + array[2]) > array[0] && (array[2] + array[0]) > array[1])
+    {
+        return ("This is valid triangle")
+    }
+    else
+    {return ("this is not valid")}
 }
-bool check_valid ()
+

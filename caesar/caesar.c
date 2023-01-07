fd7806b73eb33daa6e916c1b3af8ba345cb4c4ce
shu1513 <shu_1315@hotmail.com>
2023-01-07 22:22:57 +0000

    Sat, Jan 7, 2023, 10:22 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,14 +8,17 @@ bool only_digits (string x);
 int main(int argc, string argv[])
 {
 
-
+    //check if user only entered one input for the key
     if(argc != 2 )
     {printf("Usage: ./caesar key\n");
     return 1;}
 
+    //check if the user only entered digits
     if (only_digits (argv[1]) == false)
     {printf("Usage: ./caesar key\n");
     return 1;}
+
+
 }
 
 bool only_digits (string x)

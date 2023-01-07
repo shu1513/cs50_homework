924db86b8877cc1e700b03279ff8da2c971b9b35
shu1513 <shu_1315@hotmail.com>
2023-01-07 04:43:33 +0000

    Sat, Jan 7, 2023, 4:43 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    if(argc != 2)
+    printf("Usage: ./caesar key\n");
 
 }

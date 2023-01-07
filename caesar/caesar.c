e7faa6f74a14f827cb90d535401e9a6520e07741
shu1513 <shu_1315@hotmail.com>
2023-01-07 21:46:09 +0000

    Sat, Jan 7, 2023, 9:46 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-bool only_digits (string x)
+bool only_digits (string x);
 
 
 int main(int argc, string argv[])
@@ -13,10 +13,13 @@ int main(int argc, string argv[])
     {printf("Usage: ./caesar key\n");
     return 1;}
 
-    
-   for (int j =0; j < i; j++)
-   if ((int)argv[1][j] <48 || (int)argv[1][j]>57)
+    if (only_digits (argv[1]) = false)
     {printf("Usage: ./caesar key\n");
     return 1;}
+}
 
+bool only_digits (string x)
+{
+    for (int j =0; j < i; j++)
+   if ((int)argv[1][j] <48 || (int)argv[1][j]>57)
 }

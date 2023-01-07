d7177abcc53d0f7fd5b95b9a6c1c8f0862ea7a5b
shu1513 <shu_1315@hotmail.com>
2023-01-07 05:27:02 +0000

    Sat, Jan 7, 2023, 5:27 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,12 +7,12 @@ int main(int argc, string argv[])
 
     if(argc != 2 )
     {printf("Usage: ./caesar key\n");
-    return 1}
+    return 1;}
 
     int i = strlen (argv[1]);
    for (int j =0; j < i; j++)
    if ((int)argv[1][j] <48 || (int)argv[1][j]>57)
     {printf("Usage: ./caesar key\n");
-    return 1}
+    return 1;}
 
 }

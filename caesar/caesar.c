be355a28a86463226d7e796eaaa4bd6d938aeec3
shu1513 <shu_1315@hotmail.com>
2023-01-07 05:14:30 +0000

    Sat, Jan 7, 2023, 5:14 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,7 +5,9 @@
 int main(int argc, string argv[])
 {
     int i = strlen (argv[1]);
-    if(argc != 2 || (for (int j =0; j < i; j++) {(int)argv[j] <48 or (int)argv[j]>57}))
+    if(argc != 2 )
+    printf("Usage: ./caesar key\n");
+    else if (for (int j =0; j < i; j++) {(int)argv[j] <48 or (int)argv[j]>57})
     printf("Usage: ./caesar key\n");
 
 }

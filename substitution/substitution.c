1135feebe9d804de546b18de3dd3adac5771ecf6
shu1513 <shu_1315@hotmail.com>
2023-01-09 03:12:09 +0000

    Mon, Jan 9, 2023, 3:12 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    //check if only typed 2 things
+     if(argc != 2 )
+    {printf("Usage: ./caesar key\n");
+    return 1;}
 
 }

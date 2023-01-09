9e10ef1eae1fe28e710b2720a3c481558efcdff1
shu1513 <shu_1315@hotmail.com>
2023-01-09 03:16:15 +0000

    Mon, Jan 9, 2023, 3:16 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool only_letter (string x)
+
 int main(int argc, string argv[])
 {
     //check if only typed 2 things
@@ -8,4 +10,19 @@ int main(int argc, string argv[])
     {printf("Usage: ./substitution key\n");
     return 1;}
 
+}
+
+bool only_letter (string x)
+{
+    int i = strlen(x);
+
+    for (int j =0; j < i; j++)
+    {
+
+   if ((int)x[j] <48 || (int)x[j]>57)
+   {
+    return false;
+    }
+    }
+    return true;
 }

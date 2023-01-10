c1df55fd7ab6cbe00375be57d2bf427249da6332
shu1513 <shu_1315@hotmail.com>
2023-01-10 03:24:06 +0000

    Tue, Jan 10, 2023, 3:24 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -12,6 +12,11 @@ int main(int argc, string argv[])
     //check if the input is only letter
     if only_letter (argv[1])
     {printf("Usage: ./substitution key should only contain non-repeat alphabet\n")}
+    //make sure no letter repeats
+
+    //get plaintext
+    //assign plaintext 
+    //conver everything into upper case
 
 }
 

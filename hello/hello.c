aa4f6e53c3dd3a4375313c012f1a8d3eaba6c893
shu1513 <shu_1315@hotmail.com>
2023-01-14 20:57:09 +0000

    Sat, Jan 14, 2023, 8:57 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -12,11 +12,14 @@ int main (void)
 
 void draw(int n)
 {
-
+    if (n <=0)
+    {
+        return;
+    }
     draw(n-1);
     for (int i = 0; i < n; i++)
     {
         printf("#");
     }
-    printf("\n")
+    printf("\n");
 }

6f8cebc3940b881ae2056385b061fae8bfe49269
shu1513 <shu_1315@hotmail.com>
2023-01-16 20:51:48 +0000

    Mon, Jan 16, 2023, 8:51 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -16,27 +16,12 @@ int main (void)
 
 int collatz (int x)
 {
+    int n = 0;
+    collatz
+    //do a collatz process and add 1 to N
     //count times
     // int n = count times
 
-    int n = 0;
-    while (x != 1)
-    {
 
-    if (x % 2 == 0)
-    {
-        x = x/2;
-        n++;
-       // again loop back
-    }
-    if (x % 2 != 0 && x != 1)
-    {
-        x = x*3 +1;
-        n++;
-    }
-    // get the number to 1 using collatz
-    // get how many times it took to get to collatz
-    // return how many steps it took
-    }
     return n;
 }

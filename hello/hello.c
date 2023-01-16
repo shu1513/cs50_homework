e9206ef47f26c41fb81dc742671c81db08c660fd
shu1513 <shu_1315@hotmail.com>
2023-01-16 03:00:57 +0000

    Mon, Jan 16, 2023, 3:00 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -2,24 +2,9 @@
 #include <cs50.h>
 #include <string.h>
 
-void draw(int n);
 
 int main (void)
 {
-    int height = get_int("Enter Height here  ");
-    draw(height);
-}
-
-void draw(int n)
-{
-    if (n <=0)
-    {
-        return;
-    }
-    draw(n-1);
-    for (int i = 0; i < n; i++)
-    {
-        printf("#");
-    }
-    printf("\n");
-}
+    int n = get_int ("Enter Number Here: ")
+    
+   }

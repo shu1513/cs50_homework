915e4e67b0b0a21f79d53e6e54c88fb8b43092b7
shu1513 <shu_1315@hotmail.com>
2023-01-16 06:54:07 +0000

    Mon, Jan 16, 2023, 6:54 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -8,27 +8,33 @@ int main (void)
 {
     int n = get_int ("Enter Number Here: ");
     int a = collatz(n);
-    printf("It takes %i steps for collatz method to get your number to 1.\n")
+    printf("It takes %i steps for collatz method to get your number to 1.\n", a);
 
    }
 
 int collatz (int x)
 {
-    if x == 1;
-    (
-        return 1;
-    )
-    elif (x % 2 == 0)
+    //count times
+    // int n = count times
+
+    int n = 1;
+    while (x != 1)
     {
-        collatz(x/2);
+
+    if (x % 2 == 0)
+    {
+        x = x/2;
+        n++;
+       // again loop back
     }
-    else (x % 2 != 0)
+    if (x % 2 != 0)
     {
-        collatz(x*3+1);
+        x = x*3 +1;
+        n++;
     }
     // get the number to 1 using collatz
     // get how many times it took to get to collatz
     // return how many steps it took
+    }
     return n;
-
 }

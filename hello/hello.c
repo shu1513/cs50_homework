42e3a590bf9562bafdd75990b63705e4905fa7a6
shu1513 <shu_1315@hotmail.com>
2023-01-16 03:03:18 +0000

    Mon, Jan 16, 2023, 3:03 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -2,9 +2,17 @@
 #include <cs50.h>
 #include <string.h>
 
+int collatz (int x);
 
 int main (void)
 {
-    int n = get_int ("Enter Number Here: ")
-    
+    int n = get_int ("Enter Number Here: ");
+    int a = collatz(n);
+    printf("It takes %i steps for collatz method to get your number to 1.\n")
+
    }
+
+int collatz (int x)
+{
+
+}

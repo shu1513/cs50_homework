4e640d41d87009b17af63343606f9281528dc3b4
shu1513 <shu_1315@hotmail.com>
2023-01-21 00:35:18 +0000

    Sat, Jan 21, 2023, 12:35 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -50,7 +50,6 @@ int main(int argc, string argv[])
         }
     }
 
-    // Display winner of election
     print_winner();
 }
 
@@ -59,7 +58,6 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-
     if (strcmp(candidates[i].name, name == 0))
     {
         candidates[i].votes++;

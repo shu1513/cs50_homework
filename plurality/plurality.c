28f7c562f6ee62011f0dbadcee88ba5458d84ee0
shu1513 <shu_1315@hotmail.com>
2023-01-21 20:38:49 +0000

    Sat, Jan 21, 2023, 8:38 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -58,7 +58,7 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-    if (strcmp(candidates[i].name, name == 0))
+    if (strcmp(candidates[i].name, name) == 0)
     {
         candidates[i].votes++;
         return true;

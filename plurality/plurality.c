0843b7d1846541ea07eed47f3ae4d0dd0d8d8026
shu1513 <shu_1315@hotmail.com>
2023-01-21 00:11:25 +0000

    Sat, Jan 21, 2023, 12:11 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,7 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+
     if (strcmp(candidates[i].name, name == 0))
     {
         candidates[i].votes++;

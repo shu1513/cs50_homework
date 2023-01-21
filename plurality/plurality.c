ef2803c312c43164566ab3efccac4e4112b2a97d
shu1513 <shu_1315@hotmail.com>
2023-01-21 00:19:27 +0000

    Sat, Jan 21, 2023, 12:19 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -81,10 +81,11 @@ bool vote(string name)
 
 // Print the winner (or winners) of the election
 void print_winner(void)
-{
+{   int x;
     for (int i = 0; i < candidate_count; i++)
     {
-        if 
+        if candidates[i] > x,
+        x = candidates[i]
     }
     return;
 }

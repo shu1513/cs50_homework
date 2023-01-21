51ccad9ac95b7ae5b8a61695332613d8b9e14460
shu1513 <shu_1315@hotmail.com>
2023-01-21 04:57:40 +0000

    Sat, Jan 21, 2023, 4:57 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -72,7 +72,14 @@ void print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        candidates[i].votes
+       // candidates[i].votes biggest
+       for (int j = 0; j < candidate_count; j++)
+       {
+        if (candidates[i] >= candidates[i+1])
+        {
+            printf("%s\n",candidates[i].name)
+        }
+       }
 
         printf("%s\n",candidates[i].name)
     }

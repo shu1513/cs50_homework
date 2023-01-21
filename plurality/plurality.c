8390141d6cbff630d9c673578b262e0b1cd243ad
shu1513 <shu_1315@hotmail.com>
2023-01-21 00:29:31 +0000

    Sat, Jan 21, 2023, 12:29 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -2,10 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
-// Max number of candidates
 #define MAX 9
 
-// Candidates have name and vote count
 typedef struct
 {
     string name;
@@ -13,19 +11,15 @@ typedef struct
 }
 candidate;
 
-// Array of candidates
 candidate candidates[MAX];
 
-// Number of candidates
 int candidate_count;
 
-// Function prototypes
 bool vote(string name);
 void print_winner(void);
 
 int main(int argc, string argv[])
 {
-    // Check for invalid usage
     if (argc < 2)
     {
         printf("Usage: plurality [candidate ...]\n");

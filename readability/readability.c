d81b606d2d90dc771f519594f212e14353041d69
shu1513 <shu_1315@hotmail.com>
2023-01-23 05:15:19 +0000

    Mon, Jan 23, 2023, 5:15 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,12 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_characters(string x)
+
 int main(void)
 {
-    string x = get_string ("Text: \n")
+    string user_input = get_string ("Text: \n");
+
     //count characters
-
-    //count words
-
+    int number_of_characters = get_characters(user_input);
+    //count words using spaces
+    int number_of_words = get_words();
     //count sentences by special characters
 }

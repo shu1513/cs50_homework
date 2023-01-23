110ef567f2b2bdba4dc4587d5e6344578ab0037e
shu1513 <shu_1315@hotmail.com>
2023-01-23 07:47:58 +0000

    Mon, Jan 23, 2023, 7:47 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,21 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_characters(string x);
-int get_words(string y);
-int get_sentences(string z);
+int get_characters(string x, int a);
+int get_words(string y, int b);
+int get_sentences(string z, int c);
 int get_grade_level(int a, int b, int c)
 
 int main(void)
 {
     string user_input = get_string ("Text: \n");
-    int string_length = 
+    int string_length = strlen(user_input);
     //count characters
-    int number_of_characters = get_characters(user_input);
+    int number_of_characters = get_characters(user_input, string_length);
     //count words using spaces
-    int number_of_words = get_words(user_input);
+    int number_of_words = get_words(user_input, string_length);
     //count sentences by special characters
-    int number_of_sentences = get_sentences(user_input);
+    int number_of_sentences = get_sentences(user_input, string_length);
     //calculate grade level using formular
     int grade_level = get_grade_level(number_of_characters, number_of_words, number_of_sentences);
     void print_grade(grade_level);

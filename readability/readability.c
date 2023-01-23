6e07be858fadb32dc33404489653a055a99c48a6
shu1513 <shu_1315@hotmail.com>
2023-01-23 05:18:31 +0000

    Mon, Jan 23, 2023, 5:18 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_characters(string x)
+int get_characters(string x);
+int get_words(string y);
+int get_sentences(string z);
 
 int main(void)
 {
@@ -10,6 +12,9 @@ int main(void)
     //count characters
     int number_of_characters = get_characters(user_input);
     //count words using spaces
-    int number_of_words = get_words();
+    int number_of_words = get_words(user_input);
     //count sentences by special characters
+    int number_of_sentences = get_sentences(user_input);
+    //calculate grade level using formular 
+    int grade_level = get_grade_level(number_of_characters, number_of_words, number_of_sentences);
 }

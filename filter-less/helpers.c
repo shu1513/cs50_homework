3b0a0e4a0d984bb3f212a0951d8cbb83fb4efd76
shu1513 <shu_1315@hotmail.com>
2023-02-19 20:31:11 +0000

    Sun, Feb 19, 2023, 5:31 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int n = round((image[i][i].rgbtBlue + image[i][i].rgbtGreen + image[i][i].rgbtRed)/3);
+            int n = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3);
             image[i][j].rgbtRed = n;
             image[i][j].rgbtBlue = n;
             image[i][j].rgbtGreen = n;

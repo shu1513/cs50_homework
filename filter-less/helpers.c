a44b165f7e9776d1d4cd6f45fe662ce6f7dd9700
shu1513 <shu_1315@hotmail.com>
2023-02-19 21:53:51 +0000

    Sun, Feb 19, 2023, 6:53 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -20,6 +20,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = image[i][j].rgbtRed * 0.393 + image[i][j].rgbtGreen *0.796 +;
+            image[i][j].rgbtBlue = n;
+            image[i][j].rgbtGreen = n;
+        }
+    }
     return;
 }
 

9d14121b249806d2560d22a7d250a26bde5e2ede
shu1513 <shu_1315@hotmail.com>
2023-02-22 02:25:32 +0000

    Tue, Feb 21, 2023, 11:25 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -54,6 +54,15 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+     for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+            image[i][j] = imagine [i][|j-width|];
+
+        }
+    }
     return;
 }
 

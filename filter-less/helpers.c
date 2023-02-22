250d75169ae75d64f0487d242d6afad8c351761b
shu1513 <shu_1315@hotmail.com>
2023-02-22 03:48:34 +0000

    Wed, Feb 22, 2023, 12:48 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -70,5 +70,30 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copy[height][width] = RGBTRIPLE image[height][width]
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+            int red = roundf(image[i][j].rgbtRed * 0.393 + image[i][j].rgbtGreen *0.796 + image[i][j].rgbtBlue *0.189);
+            if (red > 255)
+            {
+                red = 255;
+            }
+
+            int green = roundf(image[i][j].rgbtRed * 0.349 + image[i][j].rgbtGreen *0.686 + image[i][j].rgbtBlue *0.168);
+            if (green > 255)
+            {
+                green = 255;
+            }
+
+            int blue = roundf(image[i][j].rgbtRed * 0.272 + image[i][j].rgbtGreen *0.534 + image[i][j].rgbtBlue *0.131);
+            if (blue > 255)
+            {
+                blue = 255;
+            }
+            image[i][j].rgbtRed = red;
+            image[i][j].rgbtGreen = green;
+            image[i][j].rgbtBlue = blue;
     return;
 }

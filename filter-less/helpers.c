305d8957b9dc8a7c7c41a95e7b045357a101c3e6
shu1513 <shu_1315@hotmail.com>
2023-02-22 03:43:34 +0000

    Wed, Feb 22, 2023, 12:43 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -69,5 +69,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width] = RGBTRIPLE image[height][width]
     return;
 }

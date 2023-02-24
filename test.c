a6241772b8f387aaba57374cbe4a9c16f859a8c1
shu1513 <shu_1315@hotmail.com>
2023-02-24 21:57:04 +0000

    Fri, Feb 24, 2023, 6:57 PM -03:00

--- test.c
+++ test.c
@@ -1,16 +1,16 @@
 # include <cs50.h>
 #include <stdio.h>
 
-void calculate (&a);
+void calcualate (int *b);
 int main(void)
 {
     int a = 20;
-    int *p = &a
+    int *p = &a;
     calcualte(p);
     printf("%i\n", a);
 }
 
-void calcualte(&a)
+void calcualte(int *b)
 {
-    a = 21;
+    b = 21;
 }

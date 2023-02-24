9f6d7fa11279042b602c878c7b89a865ed32ac9e
shu1513 <shu_1315@hotmail.com>
2023-02-24 20:56:08 +0000

    Fri, Feb 24, 2023, 5:56 PM -03:00

--- test.c
+++ test.c
@@ -1,24 +1,9 @@
 # include <cs50.h>
 #include <stdio.h>
 
-void draw(int n);
-
 int main(void)
 {
-    int a = get_int("height:  ");
-    draw(a);
-}
-
-void draw(int n)
-{
-    if (n <= 0)
-    {
-        return;
-    }
-    draw(n-1);
-    for (int i = 0; i < n; i++)
-    {
-        printf("#");
-    }
-    printf("\n");
+ int a = 20;
+    int *p = &a;
+    printf("%p", p);
 }

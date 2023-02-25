c338e1952474e9508b69ff11b3a2883f197c12de
shu1513 <shu_1315@hotmail.com>
2023-02-25 14:31:01 +0000

    Sat, Feb 25, 2023, 11:31 AM -03:00

--- pdf.c
+++ pdf.c
@@ -4,4 +4,9 @@
 int main (int argc, string argv[])
 {
     FILE *filelocation = fopen(argv[1], "r");
+    if (filelocation == NULL)
+    {
+        printf("no such file found\n");
+        return 1;
+    }
 }

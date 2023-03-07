9d61dbb8e698dfc490b0a769176759e3c0a089f3
shu1513 <shu_1315@hotmail.com>
2023-03-07 16:51:00 +0000

    Tue, Mar 7, 2023, 1:51 PM -03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -55,8 +55,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        person -> parents[0] = parent0
-        person -> parents[1] = parent1
+        person -> parents[0] = parent0;
+        person -> parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
     }
@@ -65,9 +65,12 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
+        person -> parents[0] = NULL;
+        person -> parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-
+        person -> alleles[0] = char random_allele();
+        person -> alleles[1] = char random_allele();
     }
 
     // TODO: Return newly created person

a5ff690c0471ff09cf61334e7463cf15ef24a867
shu1513 <shu_1315@hotmail.com>
2023-03-07 19:16:07 +0000

    Tue, Mar 7, 2023, 4:16 PM -03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -75,14 +75,14 @@ person *create_family(int generations)
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p == null)
+    if (p == NULL)
     {
         return;
     }

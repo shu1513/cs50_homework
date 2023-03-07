84ddde5bb7e00b29a4095f7bef7811c10c660c1e
shu1513 <shu_1315@hotmail.com>
2023-03-07 19:01:27 +0000

    Tue, Mar 7, 2023, 4:01 PM -03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -55,23 +55,23 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        person -> parents[0] = parent0;
-        person -> parents[1] = parent1;
+        p -> parents[0] = parent0;
+        p -> parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        person -> alleles[0] = parent0 -> alleles[rand()%2];
-        person -> alleles[1] = parent1 -> alleles[rand()%2];
+        p -> alleles[0] = parent0 -> alleles[rand()%2];
+        p -> alleles[1] = parent1 -> alleles[rand()%2];
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        person -> parents[0] = NULL;
-        person -> parents[1] = NULL;
+        p -> parents[0] = NULL;
+        p -> parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        person -> alleles[0] = random_allele();
-        person -> alleles[1] = random_allele();
+        p -> alleles[0] = random_allele();
+        p -> alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person

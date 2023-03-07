59809dbe8382d234a79e3940c4ef982855cba239
shu1513 <shu_1315@hotmail.com>
2023-03-07 18:57:23 +0000

    Tue, Mar 7, 2023, 3:57 PM -03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -58,7 +58,8 @@ person *create_family(int generations)
         person -> parents[0] = parent0;
         person -> parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-
+        person -> alleles[0] = parent0 -> alleles[rand()%2];
+        person -> alleles[1] = parent1 -> alleles[rand()%2];
     }
 
     // If there are no generations left to create

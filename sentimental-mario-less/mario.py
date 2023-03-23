c2734e42cadb6784f2b247eefa75e319c7a88827
shu1513 <shu_1315@hotmail.com>
2023-03-23 17:04:11 +0000

    Thu, Mar 23, 2023, 2:04 PM -03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,4 +3,4 @@ user_input = get_int("Height: ")
 while user_input <1 or user_input >8:
     user_input = get_int("Height: ")
 for i in range(user_input):
-    print(" "* i + "#" * (i+1))
+    print(" "* (user_input-i) + "#" * (i+1))

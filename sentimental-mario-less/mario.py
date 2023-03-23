5394cc3b89ea111f72510e5f0c3a4775cd2d3d36
shu1513 <shu_1315@hotmail.com>
2023-03-23 16:58:39 +0000

    Thu, Mar 23, 2023, 1:58 PM -03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,6 +1,6 @@
 from cs50 import get_int
 user_input = get_int("Height: ")
-if user_input <1 or user_input >8:
+while user_input <1 or user_input >8:
     user_input = get_int("Height: ")
 for i in range(user_input):
     print(" "* i + "#" * (i+1))

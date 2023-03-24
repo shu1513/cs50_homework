1e5836ef064a48fa520603447bcc3d041369c6bf
shu1513 <shu_1315@hotmail.com>
2023-03-24 20:03:05 +0000

    Fri, Mar 24, 2023, 5:03 PM -03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,12 +1,28 @@
+from cs50 import get_int
+
 def main():
 
 number = get_number()
 
-if check_validity
+if check_validity(number):
+    print_brand(number)
+else:
+    print("Invalid")
 
 
 
-def get_number();
+def get_number():
+    while True:
+        number = get_int("Number: ")
+        if number >0
+        break
+
+def check_validity(number):
+    
+
+def print_brand(number):
+
+
 
 
 main()

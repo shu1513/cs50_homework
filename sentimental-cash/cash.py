b25a4e1601e9b01389c3a70e417b06401cf327d3
shu1513 <shu_1315@hotmail.com>
2023-03-24 18:00:14 +0000

    Fri, Mar 24, 2023, 3:00 PM -03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,3 +1,29 @@
 import sys
 import cs50
-cents = cs50.get_int("")
+
+def main:
+
+    cents = get_cents()
+
+    quaters = get_quaters(cents)
+
+    dimes = get_dimes(cents)
+
+    nickels = get_nickels(cents)
+
+    pennies = get_pennies(cents)
+
+    coins = quaters + dimes + nickels + pennies
+
+    print(coins)
+
+def get_cents()
+    while True:
+        change_owed = cs50.get_float("Change owed: ")
+        if change_owed > 0:
+            break
+    cents = change_owed %.2 1
+
+
+
+main()

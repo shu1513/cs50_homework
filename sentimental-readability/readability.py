9af4bbd1fe4725c2002f7c1c865b371c16b8f8ee
shu1513 <shu_1315@hotmail.com>
2023-03-31 01:36:01 +0000

    Thu, Mar 30, 2023, 10:36 PM -03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,8 +1,17 @@
 def main():
 
-
+# get user input
     text = input("Text: ")
-    
 
+#count numnber of letters
+    count(text)
+#count number of words
+
+#count number of sentences
+
+#formular and round up
+#
+# print grade level
+def count(text):
 
 main()

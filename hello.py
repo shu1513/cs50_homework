3743964b5e29ac3270df1f4421b5dc11682cfa9a
shu1513 <shu_1315@hotmail.com>
2023-04-01 17:32:06 +0000

    Sat, Apr 1, 2023, 2:32 PM -03:00

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("meow\n")*3
+import csv
+with open("hello.csv", "a") as file:
+
+    name = input("name ")
+    number = input("number ")
+
+    writer = csv.DictWriter(file, fieldnames = ["name", "number"])
+    writer.writerow({"name": name, "number": number})
+
+
+

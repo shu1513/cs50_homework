5a1014a30540485e9dbd6728615194f00fff655c
shu1513 <shu_1315@hotmail.com>
2023-04-03 12:34:26 +0000

    Mon, Apr 3, 2023, 9:34 AM -03:00

--- dna/dna.py
+++ dna/dna.py
@@ -14,14 +14,14 @@ def main():
 
         # TODO: Read database file into a variable
         database=[]
-        with open(sys.argv[1]) as file:
+        with open(sys.argv[1],'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 database.append(row)
 
 
         # TODO: Read DNA sequence file into a variable
-        with open(sys.argv[2]) as file:
+        with open(sys.argv[2],'r') as file:
             sequence = f.read(file)
 
 

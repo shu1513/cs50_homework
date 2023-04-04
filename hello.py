d0547e6f1ff4a601f101848e36a2f2e020281e50
shu1513 <shu_1315@hotmail.com>
2023-04-04 03:14:20 +0000

    Tue, Apr 4, 2023, 12:14 AM -03:00

--- hello.py
+++ hello.py
@@ -1,19 +1,7 @@
 def main():
 
-    x= get_int("what's the number? ")
+    x = int(input("x here "))
     print(x)
-
-def get_int(a):
-    while True:
-        try:
-            y = int(input(a))
-
-        except ValueError:
-            pass
-        else:
-            break
-    return y
-
 
 main()
 

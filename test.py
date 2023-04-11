ece6b6d71e983f167e93154e2a6c1af0dc080a56
shu1513 <shu_1315@hotmail.com>
2023-04-11 01:24:13 +0000

    Mon, Apr 10, 2023, 10:24 PM -03:00

--- test.py
+++ test.py
@@ -1,22 +1,4 @@
 def main():
-    n = input("number here ")
-    result = digitize(n)
-    print(result)
-def digitize(n):
-
-    list = []
-
-    a = str(n)
-
-    for i in a:
-        list.append(int(i))
-
-    list2 = []
-    b = len(list)
-    while b > 0:
-        list2.append(list[b-1])
-        b = b-1
-
-    return list2
-if __name__ == "__main__":
-    main()
+    a = "hello world shu."
+    print(a.startswith(('il', 'wo', 'aweg')))
+main()

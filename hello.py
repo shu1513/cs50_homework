a2314e654dbd32f1ee03d811a7cd30fa0a165579
shu1513 <shu_1315@hotmail.com>
2023-04-12 14:33:57 +0000

    Wed, Apr 12, 2023, 11:33 AM -03:00

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 def main():
 
 
-    a = [1,2]
-    b = ['a','b']
-    c =  b +a + "a"
-    print(c)
+    a = [1,2,2,3]
+    b=a.pop(0)
+    print(b)
+    print(a)
 
 if __name__ == "__main__":
     main()

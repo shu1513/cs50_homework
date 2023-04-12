90064e881a7a3ee00dc89ad2f86ebec44b69010e
shu1513 <shu_1315@hotmail.com>
2023-04-12 20:35:40 +0000

    Wed, Apr 12, 2023, 5:35 PM -03:00

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 def main():
     a = [0,1,2,3,4,5,6,7]
-    print(a[8:0:-1])
+    for i in a:
+        if i == 2:
+            continue
+        elif i ==5:
+            break
+        else:
+            print(i)
 
 
 if __name__ == "__main__":

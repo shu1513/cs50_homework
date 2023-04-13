d952a2707c75789caaf599b1c8a3f543efe0f3df
shu1513 <shu_1315@hotmail.com>
2023-04-13 16:07:49 +0000

    Thu, Apr 13, 2023, 1:07 PM -03:00

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
 def main():
 
-    a=perfect_score([["Charles", 90], ["Tony", 80], ["Alex", 100]])
+    a=perfect_score([["Charles", 90], ["Tony", 80], ["Alex", 100], ["jame",100]])
     print(a)
 
-    def perfect_score(student_info):
-        """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
+def perfect_score(student_info):
+    """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
 
         :param student_info: list - of [<student name>, <score>] lists.
         :return: list - first `[<student name>, 100]` or `[]` if no student score of 100 is found.
         """
 
-        perfect_score_list = []
-        for student in student_info:
-            if student[1] == 100:
-                perfect_score_list.append(student)
-        return perfect_score_list
+    perfect_score_list = []
+    for student in student_info:
+        if student[1] == 100:
+            perfect_score_list.append(student)
+    return perfect_score_list
+
 
 
 if __name__ == "__main__":

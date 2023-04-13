d3bef391fb891c8d92cdfb923a74698e4b393752
shu1513 <shu_1315@hotmail.com>
2023-04-13 16:12:03 +0000

    Thu, Apr 13, 2023, 1:12 PM -03:00

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def main():
 
-    a=perfect_score([["Charles", 90], ["Tony", 80], ["Alex", 100], ["jame",100]])
+    a=perfect_score([["Charles", 90], ["Tony", 80]])
     print(a)
 
 def perfect_score(student_info):

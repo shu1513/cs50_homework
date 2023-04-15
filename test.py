5ccdb3ff6c1fbc5e628b0674224d31e40d95bbab
shu1513 <shu_1315@hotmail.com>
2023-04-15 23:17:00 +0000

    Sat, Apr 15, 2023, 8:17 PM -03:00

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 def main():
     b = {'name':'shu',"age": 33}
-    for 'name' in b:
-        print(b['name'])
+    for a in b:
+        if a == 'name':
+            print(b['name'])
 
 
 if __name__ == "__main__":

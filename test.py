ce571f852e849e2e4bb5b2b514f2830ced22a684
shu1513 <shu_1315@hotmail.com>
2023-04-21 23:22:40 +0000

    Fri, Apr 21, 2023, 8:22 PM -03:00

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 def main():
-    a = 123%10**2
+
+    a = is_armstrong_number(153)
     print(a)
 
+def is_armstrong_number(number):
+    copynumber = number
+    sum = 0
+    length = len(str(copynumber))
+    i = 1
+    while i < length + 1:
+        a = copynumber % 10**i
+        sum = sum + a**i
+        i += 1
+        copynumber = copynumber // 10
+    if sum == number:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()

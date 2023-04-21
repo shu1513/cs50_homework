7319e0835d726eb8f405387b61d042844314ab4d
shu1513 <shu_1315@hotmail.com>
2023-04-21 23:01:56 +0000

    Fri, Apr 21, 2023, 8:01 PM -03:00

--- test.py
+++ test.py
@@ -1,16 +1,6 @@
 def main():
-    is_isogram("hello")
-
-def is_isogram(string):
-    original = list(string.lower())
-    print (original)
-    new= list(set(original))
-    print (new)
-    if new == original:
-        return True
-    else:
-        return False
-
+    a = 123%100
+    print(a)
 
 
 if __name__ == "__main__":

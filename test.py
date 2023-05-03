7e1e8a4b03e6ab66f1745f2ff6831c3de79aaaeb
shu1513 <shu_1315@hotmail.com>
2023-05-03 20:24:42 +0000

    Wed, May 3, 2023, 5:24 PM -03:00

--- test.py
+++ test.py
@@ -1,17 +1,8 @@
 def main():
 
-    x=sum_of_multiples(20,[5,6])
-    print(x)
-
-def sum_of_multiples(limit, multiples):
-
-    nset = set()
-    for each in multiples:
-        i = 1
-        while each * i < limit:
-            nset.add(each * i)
-            i += 1
-    return sum(nset)
+    a ={'a':1,'b':2}
+    for i in a:
+        print(i)
 
 if __name__ == "__main__":
     main()

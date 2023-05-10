a5a71ccf417ab2ebf339d045560c63139d1c63bd
shu1513 <shu_1315@hotmail.com>
2023-05-10 15:00:10 +0000

    Wed, May 10, 2023, 12:00 PM -03:00

--- test.py
+++ test.py
@@ -7,15 +7,15 @@ def main():
 
 
     name = getstudent()
-    print(f"{name.} {name[1]}")
+    print(f"{name.n} {name.h}")
 
 
 def getstudent():
     n = "a"
     h = "b"
     Student(n,h)
-    name = Student()
-    return
+    a = Student()
+    return a
 
 if __name__ == "__main__":
     main()

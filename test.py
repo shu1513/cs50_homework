9d426d1534ff3d922c075ce02a129981fcf6bf82
shu1513 <shu_1315@hotmail.com>
2023-05-12 23:22:18 +0000

    Fri, May 12, 2023, 8:22 PM -03:00

--- test.py
+++ test.py
@@ -6,10 +6,7 @@ class Student:
 def main():
 
 
-    number = int(input("enter positive number here: "))
-    if number <0:
-        raise ShuError ("number must be positive")
-    print(number)
+    print(type(a))
 
 if __name__ == "__main__":
     main()

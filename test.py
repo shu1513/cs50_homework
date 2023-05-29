884739ab28fc3d4bbaeb439114c1a1a5a7f039b7
shu1513 <shu_1315@hotmail.com>
2023-05-29 20:25:11 +0000

    Mon, May 29, 2023, 5:25 PM -03:00

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 class Person:
     n = 0
-    def __init__(self, a,b):
-        self.a=a
-        self.b=b
+    def __init__(self, first,last):
+        self.first = first
+        self.last = last
     def sayhello(self):
         print('hello')
     @classmethod
     def sayhi(cls):
         print('hi')
+    def fullname(self):
+        return self.first, self.last
 
 class Student(Person):
     def __init__(self, a, b, c):
@@ -18,13 +20,8 @@ class Student(Person):
 
 def main():
 
-    x = Student(1,2,3)
-    Person.sayhi()
-    x.sayhello()
-    x.sayhi()
-    print(x.b)
-    print(x.c)
-
+    x = Person('Shu', 'Yang')
+    print(x.fullname())
 
 
 

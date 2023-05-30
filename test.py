41220f3bd6f00829ad3e4d6ed6b2559abb405245
shu1513 <shu_1315@hotmail.com>
2023-05-30 01:35:59 +0000

    Mon, May 29, 2023, 10:35 PM -03:00

--- test.py
+++ test.py
@@ -7,11 +7,18 @@ class Person:
         return self._fullname
     @fullname.setter
     def fullname(self, input):
-        self._fullname= input
+        if not input:
+            raise ValueError ("need to type a name here")
+        else:
+            self._fullname= input 
 
 def main():
 
     x = Person('Shu','Yang')
+    print(x.fullname)
+    x.fullname = "hi"
+    print(x.fullname)
+    x.fullname = ""
     print(x.fullname)
 
 if __name__ == "__main__":

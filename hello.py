7040f057e55dd4aee221f3ef8931e2dc464c3195
shu1513 <shu_1315@hotmail.com>
2023-06-03 02:00:04 +0000

    Fri, Jun 2, 2023, 11:00 PM -03:00

--- hello.py
+++ hello.py
@@ -16,11 +16,23 @@ class Sentence:
 
 
 def main():
-    my_sentence = Sentence('This is a test')
-    for word in my_sentence:
-        print(word)
+
+    m
 
 def sentence(string):
+    pass
+
+def my_range(start, end):
+    current = start
+    while current < end:
+        yield current
+        current += 1
+
+def sentence(string):
+    stringlist=string.split()
+    i = 0
+    while i < len(stringlist)
+        yield len[i]
 
 
 

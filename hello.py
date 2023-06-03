7dd65abeb1cddbdf0edf31fadd221754d2e860ba
shu1513 <shu_1315@hotmail.com>
2023-06-03 00:52:03 +0000

    Fri, Jun 2, 2023, 9:52 PM -03:00

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 class Sentence:
     def __init__(self,string):
+        self = string.split()
 
 
 def main():
+    my_sentence = Sentence('This is a test')
+    for word in my_sentence:
+        print(word)
 
 def sentence(string):
     for word in string.split():

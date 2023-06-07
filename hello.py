7a6b4d709d5baafb504f56aacc7b3c39be5672cf
shu1513 <shu_1315@hotmail.com>
2023-06-07 19:51:36 +0000

    Wed, Jun 7, 2023, 4:51 PM -03:00

--- hello.py
+++ hello.py
@@ -1,41 +1,12 @@
-class Sentence:
-    def __init__(self,string):
-        self.content = string.split()
-        self.i = 0
-    def __iter__(self):
-        return self
-    def __next__(self):
 
-        if self.i >= len(self.content):
-            raise StopIteration
-        else:
-            word = self.content[self.i]
-            self.i += 1
-            return word
 
 
 
 def main():
 
-    my_sentence = sentence('This is a test')
-    for word in my_sentence:
-        print(word)
+   x=2
 
-
-def my_range(start, end):
-    current = start
-    while current < end:
-        yield current
-        current += 1
-
-def sentence(string):
-    stringlist=string.split()
-    i = 0
-    while i < len(stringlist):
-        yield stringlist[i]
-        i += 1
-
-
+   print('a', str(lambda x: x**2))
 
 
 

e156e21263e2502040b973c2efc35262608b0815
shu1513 <shu_1315@hotmail.com>
2023-06-08 04:29:56 +0000

    Thu, Jun 8, 2023, 1:29 AM -03:00

--- test_hello.py
+++ test_hello.py
@@ -1,9 +1,17 @@
-from hello import sayhello
+import hello
 
 import unittest
 
 class HelloTest(unittest.TestCase):
     def test_sayhello(self):
-        self.assertEqual(sayhello('shu'), 'hello, shu')
+        self.assertEqual(hello.sayhello('shu'), 'hello, shu')
+
+    def test_square(self):
+        self.assertEqual(hello.square(-2), 4)
+
+    def test_square_zero(self):
+        self.assertEqual(hello.square(0),0)
+
+
 if __name__ == "__main__":
     unittest.main(verbosity=1)

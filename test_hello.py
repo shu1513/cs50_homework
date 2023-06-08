b045e3a25d14ab7f1605ecef69171c7d16540769
shu1513 <shu_1315@hotmail.com>
2023-06-08 04:23:07 +0000

    Thu, Jun 8, 2023, 1:23 AM -03:00

--- test_hello.py
+++ test_hello.py
@@ -5,5 +5,5 @@ import unittest
 class HelloTest(unittest.TestCase):
     def test_sayhello(self):
         self.assertEqual(sayhello('shu'), 'hello shu')
-if __name__ = "__main__":
+if __name__ == "__main__":
     unittest.main

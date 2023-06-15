bc83b3a1bc85805528c8e239f24c14c8520dbb51
shu1513 <shu_1315@hotmail.com>
2023-06-15 04:32:51 +0000

    Thu, Jun 15, 2023, 1:32 AM -03:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,1 +1,1 @@
-ELECT name FROM songs where artist_id = (SELECT id FROM artists WHERE name = 'Post Malone');
+SELECT name FROM songs where artist_id = (SELECT id FROM artists WHERE name = 'Post Malone');

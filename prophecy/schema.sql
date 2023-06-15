5e00b0ea8bfd3bd930c51bd901c4c5ea058dda57
shu1513 <shu_1315@hotmail.com>
2023-06-15 23:26:37 +0000

    Thu, Jun 15, 2023, 8:26 PM -03:00

--- prophecy/schema.sql
+++ prophecy/schema.sql
@@ -1,3 +1,5 @@
-CREATE TABLE students (id INTEGER, student_name TEXT, PRIMARY KEY(id));
+CREATE TABLE students_names (id INTEGER, student_name TEXT, PRIMARY KEY(id));
 CREATE TABLE houses (id INTEGER, house TEXT, head TEXT, PRIMARY KEY(id));
-CREATE TABLE house_assignments (student_id INTEGER, house_id INTEGER, FOREIGN KEY(students.id), FOREIGN KEY(houses.id));
+CREATE TABLE house_assignments (student_id INTEGER, house_id INTEGER,
+FOREIGN KEY(student_id), REFERENCES student_names(id)
+FOREIGN KEY(house_id));

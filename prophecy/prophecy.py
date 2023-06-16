1235a96fe1b47abbabef5726472d5cb59e50db41
shu1513 <shu_1315@hotmail.com>
2023-06-16 01:41:00 +0000

    Thu, Jun 15, 2023, 10:41 PM -03:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -1,6 +1,16 @@
 import csv
 from cs50 import SQL
 
-open('final.db', 'w').close()
+open("final.db", "w").close()
 db = SQL("sqlite:///final.db")
-db.execute('CREATE TABLE stunames(id INTEGER NOT NULL, name TEXT NOT NULL, PRIMARY KEY(id))')
+db.execute(
+    "CREATE TABLE stunames(id INTEGER NOT NULL, name TEXT NOT NULL, PRIMARY KEY(id))"
+)
+db.execute(
+    "CREATE TABLE houses(id INTEGER NOT NULL, house TEXT NOT NULL, PRIMARY KEY(id))"
+)
+db.execute(
+    "CREATE TABLE assignments(student_id INTEGER NOT NULL, house_id INTEGER NOT NULL, FOREIGN KEY(student_id) REFERENCES stunames(id), FOREIGN KEY(house_id) REFERENCES houses(id))"
+)
+
+

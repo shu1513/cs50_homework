337e1d7e7aba8ed8f42735ed93896d3fcf3862e6
shu1513 <shu_1315@hotmail.com>
2023-06-19 14:52:56 +0000

    Mon, Jun 19, 2023, 11:52 AM -03:00

---
+++ movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON people.id = stars.person_id JOIN ratings ON ratings.movie_id = movies.id Where name = 'Chadwick Boseman' ORDER BY rating DESC LIMIT 5;

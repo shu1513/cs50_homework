e0346f9bbf9157e04f437d45fee7fe64a7e98772
shu1513 <shu_1315@hotmail.com>
2023-06-23 19:59:25 +0000

    Fri, Jun 23, 2023, 4:59 PM -03:00

--- hello.sql
+++ hello.sql
@@ -1,2 +1,3 @@
 CREATE TABLE IF NOT EXISTS hello (id INTEGER, name TEXT, number INTEGER, PRIMARY KEY(id));
 CREATE TABLE IF NOT EXISTS station (id INTEGER, name TEXT, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS relationships(hello_id INTEGER, station_id INTEGER, FOREIGN KEY(hello_id) REFERENCES hello(id), FOREIGN KEY(station_id))

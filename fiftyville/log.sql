2a9f8cf6b421d58a7499e74cc9fb7f100424ce36
shu1513 <shu_1315@hotmail.com>
2023-06-26 03:31:30 +0000

    Mon, Jun 26, 2023, 12:31 AM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,13 @@
 Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
 I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
 As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
+| id  | year | month | day | hour | minute | activity | license_plate |
++-----+------+-------+-----+------+--------+----------+---------------+
+| 260 | 2021 | 7     | 28  | 10   | 16     | exit     | 5P2BI95       |
+| 261 | 2021 | 7     | 28  | 10   | 18     | exit     | 94KL13X       |
+| 262 | 2021 | 7     | 28  | 10   | 18     | exit     | 6P58WS2       |
+| 263 | 2021 | 7     | 28  | 10   | 19     | exit     | 4328GD8       |
+| 264 | 2021 | 7     | 28  | 10   | 20     | exit     | G412CB7       |
+| 265 | 2021 | 7     | 28  | 10   | 21     | exit     | L93JTIZ       |
+| 266 | 2021 | 7     | 28  | 10   | 23     | exit     | 322W7JE       |
+| 267 | 2021 | 7     | 28  | 10   | 23     | exit     | 0NTHK55   

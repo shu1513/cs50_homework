932c94bbe505a0b792aaa0cefb638b324afa0fe8
shu1513 <shu_1315@hotmail.com>
2023-11-15 19:48:24 +0000

    Wed, Nov 15, 2023, 4:48 PM -03:00

--- finance/app.py
+++ finance/app.py
@@ -121,6 +121,10 @@ def register():
         if request.method == "POST":
             if not request.form.get("username"):
                 return apology("must provide username", 403)
+            elif not request.form.get("password"):
+                return apology("must provide password", 403)
+            else:
+                
 
     return apology("TODO")
 

b30f567e75330daa80f419519a3a5b5571ae311e
shu1513 <shu_1315@hotmail.com>
2023-12-13 02:06:33 +0000

    Tue, Dec 12, 2023, 6:06 PM -08:00

--- finance/app.py
+++ finance/app.py
@@ -147,7 +147,7 @@ def register():
                 or not any(char.isupper() for char in password)
                 or not any(char.islower() for char in password)
                 or not any(char.isdigit() for char in password)
-                or not any(char in "@$!%*?&" for char in password)
+                or not any(char in string.punctuation for char in password)
             ):
                 return apology(
                     "passwords must be a between 8 to 16 digits long, contain one upper case letter, one lower case letter, one numeric digit, and one special character (example: @$!%*?&)",

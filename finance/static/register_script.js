c1555147adb0d7e52da8f2ecd53e420469fd188b
shu1513 <shu_1315@hotmail.com>
2023-12-13 06:27:53 +0000

    Tue, Dec 12, 2023, 10:27 PM -08:00

--- finance/static/register_script.js
+++ finance/static/register_script.js
@@ -37,9 +37,13 @@
 
   function passwordGuide() {
     const password = document.getElementByID("password").value;
-    const passwordGuide = document.getElementByID("passwordGuide").value;
+    const passwordGuide = document.getElementById("passwordGuide").value;
     if (!re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]', password)) {
-      passwordguide.innerht
+      passwordguide.innerHTML= "password should be 8-16 long containing contains at least one lowercase letter, one uppercase letter, one digit, and one symbol."
+      passwordguide.style.color="red"
     }
-
+    else {
+      passwordguide.innerHTML= "password should be 8-16 long containing contains at least one lowercase letter, one uppercase letter, one digit, and one symbol."
+      passwordguide.style.color="green"
+    }
   }

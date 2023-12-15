7dce4682975862406d9c68471df2457251b9ac9b
shu1513 <shu_1315@hotmail.com>
2023-12-15 07:57:45 +0000

    Thu, Dec 14, 2023, 11:57 PM -08:00

--- finance/static/register_script.js
+++ finance/static/register_script.js
@@ -1,22 +1,35 @@
   //javacript for password confirmations in the register.html page.
 
-  import sqlite3 from 'sqlite3';
+  const sqlite3 = require('sqlite3').verbose();
+  const db = new sqlite3.Database('finance.db');
 
-  function usernameDuplicate() {
-    const db = new sqlite3.Database('finance.db');
-    const username = document.getElementById("username").value;
-    const usernames_list = db.all("SELECT * FROM users");
+  function checkUsernameDuplicate() {
+      const username = document.getElementById("username").value;
 
-    for name in usernames_list:
-    if name["username"] = username:
-    document.getElementById("usernameDuplicate").innerHTML = "This username already exist";
-    document.getElementById("usernameDuplicate").style.color = "red";
+      // Fetch all usernames from the database
+      db.all("SELECT * FROM users", (err, usernames_list) => {
+          if (err) {
+              console.error(err.message);
+              return;
+          }
 
-    if (username in usernames_list) {
-      document.getElementById("usernameDuplicate").innerHTML = "This username already exist";
-      document.getElementById("usernameDuplicate").style.color = "red";
-    }
-  }
+          // Check if the entered username already exists
+          const isDuplicate = usernames_list.some(name => name.username === username);
+
+          if (isDuplicate) {
+              // Display an error message if the username already exists
+              document.getElementById("usernameDuplicate").innerHTML = "This username already exists";
+              document.getElementById("usernameDuplicate").style.color = "red";
+          } else {
+              // Clear the error message if the username is not a duplicate
+              document.getElementById("usernameDuplicate").innerHTML = "";
+                  }
+          });
+          }
+
+  // Example of how to call the function (e.g., on a button click)
+  // document.getElementById("checkUsernameButton").addEventListener("click", checkUsernameDuplicate);
+
 
   function validateForm() {
     const password = document.getElementById("password").value;

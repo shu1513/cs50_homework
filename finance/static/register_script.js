4dc3ef98a527c3b8f1618a9bf83f7dc0924d48bf
shu1513 <shu_1315@hotmail.com>
2023-12-15 23:44:54 +0000

    Fri, Dec 15, 2023, 3:44 PM -08:00

--- finance/static/register_script.js
+++ finance/static/register_script.js
@@ -82,6 +82,9 @@
     const passwordGuide1 = document.getElementById("passwordGuide1");
     const passwordGuide2 = document.getElementById("passwordGuide2");
     const passwordGuide3 = document.getElementById("passwordGuide3");
+    passwordGuide1.innerHTML = "<span class='invalid'>&#10008;</span> 8-16 characters";
+    passwordGuide2.innerHTML ="<span class='invalid'>&#10008;</span> at least 1 uppercase and 1 lowercase letter";
+    passwordGuide3.innerHTML ="<span class='invalid'>&#10008;</span> 1 digit, and 1 symbol (ex: @ ! $ = )";
 
     if (password.length >= 8 && password.length <+ 16) {
       passwordGuide1.innerHTML ="<span class='valid'>&#10004;</span>8-16 characters";
@@ -92,10 +95,5 @@
     if (/\d/.test(password) && /\W/.test(password)) {
       passwordGuide3.innerHTML ="<span class='valid'>&#10004;</span> 1 digit, and 1 symbol (ex: @ ! $ = )";
     }
-    else {
 
-      passwordGuide1.innerHTML = "<span class='invalid'>&#10008;</span> 8-16 characters";
-      passwordGuide2.innerHTML ="<span class='invalid'>&#10008;</span> at least 1 uppercase and 1 lowercase letter";
-      passwordGuide3.innerHTML ="<span class='invalid'>&#10008;</span> 1 digit, and 1 symbol (ex: @ ! $ = )";
-    }
   }

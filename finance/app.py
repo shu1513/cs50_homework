8590d1957af2a6eb6ae4e07f981be51dd9ff530c
shu1513 <shu_1315@hotmail.com>
2023-12-16 22:29:41 +0000

    Sat, Dec 16, 2023, 2:29 PM -08:00

--- finance/app.py
+++ finance/app.py
@@ -117,8 +117,10 @@ def logout():
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
 def quote():
-    """Get stock quote."""
-    return apology("TODO")
+    if request.method == "POST":
+        
+    else:
+        return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])

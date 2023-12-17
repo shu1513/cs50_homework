a68e051d0d74d5f32adf356a53722b34658131c9
shu1513 <shu_1315@hotmail.com>
2023-12-17 00:13:20 +0000

    Sat, Dec 16, 2023, 4:13 PM -08:00

--- finance/app.py
+++ finance/app.py
@@ -118,7 +118,7 @@ def logout():
 @login_required
 def quote():
     if request.method == "POST":
-        return render_template("quote.html")
+        return apology("TODO")
     else:
         return render_template("quote.html")
 

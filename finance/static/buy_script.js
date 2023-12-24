2469552274831011f842034be5f14a7ab56b78b9
shu1513 <shu_1315@hotmail.com>
2023-12-24 00:51:49 +0000

    Sat, Dec 23, 2023, 4:51 PM -08:00

--- finance/static/buy_script.js
+++ finance/static/buy_script.js
@@ -1,5 +1,5 @@
 function checkShares() {
-    const shares = document.getElementById("shares").value;
+    let shares = document.getElementById("shares").value;
     if (isNaN(shares)) {
         document.getElementById("sharesGuide").innerHTML = "<span>&#10008;</span> must be numbers"
         document.getElementById("sharesGuide").style.color = "red"

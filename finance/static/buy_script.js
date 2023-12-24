eb64b2227c86569d350df618603b31323493a2e6
shu1513 <shu_1315@hotmail.com>
2023-12-24 01:10:49 +0000

    Sat, Dec 23, 2023, 5:10 PM -08:00

--- finance/static/buy_script.js
+++ finance/static/buy_script.js
@@ -1,8 +1,8 @@
 //this doesn't work with when html type is numbers
 function checkShares() {
-    const shares = parseFloat(document.getElementById("shares").value);
-    if (isNaN(shares)) {
-        document.getElementById("sharesGuide").innerHTML = "<span>&#10008;</span> must be numbers"
+    const shares = parseInt(document.getElementById("shares").value);
+    if (isNaN(shares) && shares >= 0) {
+        document.getElementById("sharesGuide").innerHTML = "<span>&#10008;</span> numbers of shares must be an integer bigger than 1"
         document.getElementById("sharesGuide").style.color = "red"
     }
     else {

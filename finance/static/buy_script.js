1b89611ffce21071048883d32e772f75032a7d7a
shu1513 <shu_1315@hotmail.com>
2023-12-24 01:03:35 +0000

    Sat, Dec 23, 2023, 5:03 PM -08:00

--- finance/static/buy_script.js
+++ finance/static/buy_script.js
@@ -1,12 +1,12 @@
-//this doesn't work
+//this doesn't work with when html type is numbers
 function checkShares() {
     const shares = document.getElementById("shares").value;
-    if (shares) {
+    if (isNaN(shares)) {
         document.getElementById("sharesGuide").innerHTML = "<span>&#10008;</span> must be numbers"
         document.getElementById("sharesGuide").style.color = "red"
     }
     else {
-        document.getElementById("sharesGuide").innerHTML = "<span>&#10004;</span> must be numbers"
+        document.getElementById("sharesGuide").innerHTML = "<span>&#10004;</span>"
         document.getElementById("sharesGuide").style.color = "green"
     }
 }

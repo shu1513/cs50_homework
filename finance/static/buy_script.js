9dfc07f204573b29c0aa78c5b0dffa3947282ffa
shu1513 <shu_1315@hotmail.com>
2023-12-24 01:01:01 +0000

    Sat, Dec 23, 2023, 5:01 PM -08:00

--- finance/static/buy_script.js
+++ finance/static/buy_script.js
@@ -1,10 +1,12 @@
+//this doesn't work
 function checkShares() {
     const shares = document.getElementById("shares").value;
-    if (shares.isNaN()) {
+    if (shares) {
         document.getElementById("sharesGuide").innerHTML = "<span>&#10008;</span> must be numbers"
         document.getElementById("sharesGuide").style.color = "red"
     }
     else {
-        document.getElementById("sharesGuide").innerHTML = "hi"
-}
+        document.getElementById("sharesGuide").innerHTML = "<span>&#10004;</span> must be numbers"
+        document.getElementById("sharesGuide").style.color = "green"
+    }
 }

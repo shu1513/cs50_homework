10d2e42614f75da8080ac5a3f67bfe300f97fa27
shu1513 <shu_1315@hotmail.com>
2023-12-24 01:06:29 +0000

    Sat, Dec 23, 2023, 5:06 PM -08:00

--- finance/static/buy_script.js
+++ finance/static/buy_script.js
@@ -1,6 +1,6 @@
 //this doesn't work with when html type is numbers
 function checkShares() {
-    const shares = document.getElementById("shares").value;
+    const shares = parseFloat(document.getElementById("shares").value);
     if (isNaN(shares)) {
         document.getElementById("sharesGuide").innerHTML = "<span>&#10008;</span> must be numbers"
         document.getElementById("sharesGuide").style.color = "red"
